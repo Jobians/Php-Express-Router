40eddc5dce63cfc5be3f0167bd210573bfcd144a
Jobians Techie <88005779+Jobians@users.noreply.github.com>
2023-08-24 17:17:47 +0100

    Update server.php

--- examples/defining_routes/server.php
+++ examples/defining_routes/server.php
@@ -4,6 +4,7 @@ require_once '../../core/Router.php';
 
 $router = new Router();
 
+// Define routes for various HTTP methods
 $router->get('/', function ($req, $res) {
     $res->send('Welcome to our website! (GET)');
 });
@@ -28,9 +29,43 @@ $router->options('/', function ($req, $res) {
     $res->send('Options requested! (OPTIONS)');
 });
 
+// Define a fallback route for all HTTP methods
 $router->all('/fallback', function ($req, $res) {
     $method = $req->method;
     $res->send("Fallback route for $method requests");
 });
 
+// Define a route with a parameter
+$router->get('/users/:id', function ($req, $res) {
+    $userId = $req->params['id'];
+    $res->send("User with ID $userId requested");
+});
+
+// Route with a parameter for a specific user
+$router->get('/users/:username', function ($req, $res) {
+    $username = $req->params['username'];
+    $res->send("Profile of user: $username");
+});
+
+// Route with multiple parameters
+$router->get('/products/:category/:product_id', function ($req, $res) {
+    $category = $req->params['category'];
+    $productId = $req->params['product_id'];
+    $res->send("Product details - Category: $category, Product ID: $productId");
+});
+
+// Route with a wildcard for capturing the rest of the URL
+$router->get('/articles/*', function ($req, $res) {
+    $path = $req->params[0];
+    $res->send("Requested article path: $path");
+});
+
+// Route with a wildcard and a parameter
+$router->get('/categories/:category/*', function ($req, $res) {
+    $category = $req->params['category'];
+    $path = $req->params[0];
+    $res->send("Requested category: $category, Article path: $path");
+});
+
 $router->run();
+?>
